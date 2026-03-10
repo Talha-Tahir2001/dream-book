7b600c68b36caa540e5c37ff1693693bd981f11b
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2026-03-10 12:44:56 +0500

    initial commit from the NestJS CLI

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const logger = app.get(Logger);
+  // app.setGlobalPrefix('api');
+  const origin =
+    configService.get<string>('FRONTEND_ORIGIN') || 'http://localhost:3000';
+  logger.log(`CORS Origin set to: ${origin}`);
+  app.enableCors({ origin });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  app.enableShutdownHooks();
+  const port = configService.get<number>('PORT') || 8000;
+  await app.listen(port);
+  logger.log(`Application is running on: http://localhost:${port}`);
+}
+bootstrap().catch((error) => {
+  console.error('Failed to bootstrap application:', error);
+  process.exit(1);
+});
