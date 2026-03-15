51a7b72f2f824445ac8fcce62f920beb95158735
Talha Tahir <140109354+Talha-Tahir2001@users.noreply.github.com>
2026-03-16 00:01:41 +0500

    chore: update all the imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { FirebaseModule } from './firebase/firebase.module';
+import { GcsModule } from './gcs/gcs.module';
+import { StoryModule } from './story/story.module';
+import { PdfModule } from './pdf/pdf.module';
+import { HealthModule } from './health/health.module';
+import { InternalModule } from './internal/internal.module';
+import { GeminiModule } from './gemini/gemini.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    FirebaseModule,
+    GcsModule,
+    StoryModule,
+    GeminiModule,
+    PdfModule,
+    HealthModule,
+    InternalModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
